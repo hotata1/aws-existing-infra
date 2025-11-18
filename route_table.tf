abe2ea9b54c6d1afd300fd69ed6ba8fa8f14b338
Hotata <onoshota39@gmail.com>
2025-11-18 17:03:21 +0900

    feat: Import Default Route (0.0.0.0/0) to IGW

--- route_table.tf
+++ route_table.tf
@@ -8,3 +8,9 @@ resource "aws_route_table" "main_route_table" {
     # 既存のルートテーブルにタグがあれば追記
   }
 }
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.main_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_igw.id
+}
